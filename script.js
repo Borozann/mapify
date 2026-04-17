c7d4c6c6c14a270e85944645338dc0ead1cb111c
Borozann <gabriel.borozan83@gmail.com>
2026-04-17 12:55:54 -0300

    refactor: implement Workout, Running, and Cycling classes for better structure and functionality

--- script.js
+++ script.js
@@ -13,6 +13,48 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
+class Workout {
+  date = new Date();
+  id = (new Date() + '').slice(-10, -1);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Running([39, -12], 27, 95, 523);
+console.log(run1);
+console.log(cycling1);
+
+// APPLICATION ARCHITETURE
 class App {
   #map;
   #mapEvent;
